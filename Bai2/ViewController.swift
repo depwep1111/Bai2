af85de30f2e57de458656b031508732387f6d383
Cntt06 <Admin06@Cntt06s-iMac.local>
2017-07-05 14:21:20 +0700

    first

--- Bai2/ViewController.swift
+++ Bai2/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var img2: UIImageView!
+    @IBOutlet weak var txtUrl1: UITextField!
+    @IBOutlet weak var txtUrl2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnTai(_ sender: Any) {
+        let imageURL: URL = URL(string: self.txtUrl1.text!)!
+        
+        (URLSession(configuration: URLSessionConfiguration.default)).dataTask(with: imageURL, completionHandler: { (imageData, response, error) in
+            
+            if let data = imageData {
+                DispatchQueue.main.async {
+                    self.img1.image = UIImage(data: data)
+                }
+            }
+        }).resume()
+    }
+    
+    @IBAction func btnTai2(_ sender: Any) {
+        let imageURL: URL = URL(string: self.txtUrl2.text!)!
+        
+        (URLSession(configuration: URLSessionConfiguration.default)).dataTask(with: imageURL, completionHandler: { (imageData, response, error) in
+            
+            if let data = imageData {
+                DispatchQueue.main.async {
+                    self.img2.image = UIImage(data: data)
+                }
+            }
+        }).resume()
+    }
 }
 
